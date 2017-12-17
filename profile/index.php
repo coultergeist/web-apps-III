9b85cf11ebfb64f5f1ec7aff85141e22b9df5b97
DM Coulter <dcoult01@seattlecentral.edu>
2017-12-17 08:12:26 -0800

    Profile upload
    Upload profile file page for Web Apps III class built from scratch: HTML5/CSS/PHP

---
+++ profile/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Danielle Coulter's Profile Page</title>
+<!--Remy Sharp Shim --> 
+<!--[if lt IE 9]> 
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js">
+</script> 
+<![endif]-->
+
+<link href="css/styles.css" type="text/css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+<h1>Danielle Coulter's Profile Page</h1>
+
+<h2>ITC 260 @ Seattle Central</h2>
+
+<img src="images/me.jpg" alt="me">
+
+<h3>Biography</h3>
+
+<p>My name is Danielle but you can just call me <strong>"D."</strong> I've been at Seattle Central since the Winter of 2016. I came here after living in Japan for two years. I have two cats and a dog, all rescues.</p>
+
+<p>This is a career change for me. My background ranges from food service to military. I'm new to Seattle and am just celebrating my second summer here. I'm finishing my WebDev AA here at Central and moving on to North for their AppDev BA program.</p>
+
+<p>In the realm of development, I'm best at (or should I say most captured by) PHP. I love how you can connect everything together, databases, HTML, JavaScript. PHP is so dynamic! But I have to say that I do have an (unrequited) love for Java as well. And a fascination with databases. I'm in the WebDev degree program but it seems that I haven't written anything in HTML/CSS in a year, or so it feels that way.</p> 
+    
+<p>I'm not as good at JavaScript. And I'm not an expert in Java though I do like it. I'm doing the trick of putting small programming tasks into a bowl and pulling one at random each day to learn coding. This summer I'm also doing my Capstone so I plan to practice a lot more than I usually get the opportunity for.</p>
+    
+<p>I'm not planning to meet in class unless it's required. If any group(s) I'm a part of would like to meet in person I'm happy to. Feel free to email here, on Canvas, or on my personal email at dcoult01@seattlecentral.edu.</p>
+
+<p>Here's my <a href="https://github.com/coultergeist" >GITHUB!</a></p>
+
+<hr />
+
+
+</div>
+
+</body>
+
+</html>
